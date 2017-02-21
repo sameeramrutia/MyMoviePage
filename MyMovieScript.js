a7f3e9660f74305ff11f916a413fcfd5d8f51b53
Sameer <sameeramrutia@gmail.com>
2017-02-22 01:06:43 +0530

    Add files via upload

---
+++ MyMovieScript.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+
+angular.module('myApp', [])
+  .controller('MovieController', function ($scope, $http, MyMovieService) {
+      $scope.$watch('search', function () {
+          //fetch();
+          $scope.getData();
+          getRelatedSearch();
+      });
+
+      $scope.search = "Sherlock Holmes";
+
+      function fetch() {
+          $http.get("https://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+            .then(function (response) {
+                $scope.details = response.data;
+            });
+
+          $http.get("https://www.omdbapi.com/?s=" + $scope.search)
+            .then(function (response) {
+                $scope.related = response.data;
+            });
+      }
+
+      $scope.update = function (movie) {
+          $scope.search = movie.Title;
+      };
+
+      $scope.select = function () {
+          //this.setSelectionRange(0, this.value.length);
+          $scope.getData();
+          getRelatedSearch();
+      }
+      $scope.getData = function () {
+          var data = null;
+          data = MyMovieService.getMovieTitleDetails($scope.search).then(function (data) {
+              $scope.details = data;
+            });
+      }
+      function getRelatedSearch() {
+          var data = null;
+          data = MyMovieService.getMovieTitleRelatedSearch($scope.search).then(function (data) {
+              $scope.related = data;
+          });
+      }
+  });
---
+++ MyMovieService.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    angular.module('myApp').factory("MyMovieService", MyMovieService);
+
+    MyMovieService.$inject = ['$http'];
+    function MyMovieService($http) {
+        return {
+            getMovieTitleDetails: getMovieTitleDetails,
+            getMovieTitleRelatedSearch: getMovieTitleRelatedSearch
+        };
+        function getMovieTitleDetails(searchTerms) {
+            return $http.get("https://www.omdbapi.com/?t=" + searchTerms + "&tomatoes=true&plot=full")
+               .then(function (response) {
+                   return response.data;
+               });
+        };
+        function getMovieTitleRelatedSearch(searchTerms) {
+            return $http.get("https://www.omdbapi.com/?s=" + searchTerms)
+               .then(function (response) {
+                   return response.data;
+               });
+        };
+    };
+}());
+
+
